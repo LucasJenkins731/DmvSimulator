d07ae82d6a428dc687422d329601718675920c83
LucasJenkins731 <lgjenkins@quinnipiac.edu>
2025-02-26 19:30:11 -0500

    Gives user a random number

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -2,6 +2,11 @@ import java.util.*;
 
 public class DmvSimulator {
     public static void main(String[] args) {
+        Random random = new Random();
         System.out.println("Welcome to the DMV!");
+
+        int number = random.nextInt(100);
+
+        System.out.println("Your number is " + number + ". Please wait until your number is called.");
     }
 }

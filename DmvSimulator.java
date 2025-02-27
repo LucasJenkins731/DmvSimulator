302a77ae149cd2707fd3d6146737bb3d8c0ecacf
LucasJenkins731 <lgjenkins@quinnipiac.edu>
2025-02-26 19:43:30 -0500

    Added called numbers

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -5,8 +5,22 @@ public class DmvSimulator {
         Random random = new Random();
         System.out.println("Welcome to the DMV!");
 
-        int number = random.nextInt(100);
+        int range = 100;
+        int number = random.nextInt(range);
 
         System.out.println("Your number is " + number + ". Please wait until your number is called.");
+
+        int wait = number + 1;
+
+        while (wait != number) {
+            System.out.println("Called number: " + wait);
+            if (wait > range) {
+                wait = 1;
+            }
+            
+            wait++;
+        }
+        System.out.println("Called number: " + number);
+        System.out.println("Thank you for waiting!");
     }
 }
